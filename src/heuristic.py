1bc4f6163a9f05e9eb1785d86f9f6cf874e67e7b
MarcosFP97 <marcosfernandez.pichel@usc.es>
2024-01-19 10:58:22 +0100

    Llama experiments

--- src/heuristic.py
+++ src/heuristic.py
@@ -8,9 +8,12 @@ from bleurt import score
 from rouge_metric import PyRouge
 import numpy as np
 import sys
+from scipy.stats import wilcoxon
+from transformers import LlamaTokenizer
 
-model = "gpt-3.5-turbo"
-openai.api_key = "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"
+model = "../models/llama-2-13b-chat.Q8_0.gguf"
+openai.api_base = "http://localhost:8000/v1"
+openai.api_key = ""
 checkpoint = "BLEURT-20"
 scorer = score.BleurtScorer(checkpoint)
 rouge = PyRouge()
@@ -32,15 +35,6 @@ def load_benchmark(
 
     return topics
 
-# def splitter(
-#     point:str
-# ) -> tuple:
-#     enc = tiktoken.encoding_for_model(model)
-#     q = enc.encode(point)
-#     q1 = enc.decode(q[:len(q)//2])
-#     q2 = enc.decode(q[len(q)//2:])
-#     return q1,q2
-
 def sim(
    completion:str,
    q2:str     
@@ -52,38 +46,71 @@ def complete(
     title:str,
     question:str,
     answer:str,
-    fields:tuple    
+    fields:tuple,
+    max_tokens:int   
 ) -> str:
-    guided_prompt = f'You are provided with the {fields[0]}, {fields[1]} and {fields[2]} fields of a topic from the TREC {year} Health Misinformation topic set.'\
-    f'Complete the {fields[3]} field of the topic as exactly appeared in the dataset.'\
+    guided_prompt = f'<s>[INST] <<SYS>>\n<</SYS>>\nYou are provided with the {fields[0]}, {fields[1]} and {fields[2]} fields of a topic from the TREC {year} Health Misinformation topic set. '\
+    f'Complete the {fields[3]} field of the topic as exactly appeared in the dataset. '\
     f'Only rely on the original form of the topic in the dataset to complete the narrative field.'\
-    f'\n{fields[0]}:{title}\n{fields[1]}:{question}\n{fields[2]}:{answer}\n{fields[3]}:' 
-    general_prompt = f'Complete the {fields[3]} based on the {fields[0]}, {fields[1]} and {fields[2]} fields such that the {fields[3]} provides an explanation for the {fields[2]} to the given {fields[1]}'\
-    f'\n{fields[0]}:{title}\n{fields[1]}:{question}\n{fields[2]}:{answer}\n{fields[3]}:' 
+    f'\n{fields[0]}: {title}\n{fields[1]}: {question}\n{fields[2]}: {answer} \n{fields[3]}: [\INST]' 
+    general_prompt = f'<s>[INST] <<SYS>>\n<</SYS>>\nComplete the {fields[3]} based on the {fields[0]}, {fields[1]} and {fields[2]} fields such that the {fields[3]} provides an explanation for the {fields[2]} to the given {fields[1]}.'\
+    f'\n{fields[0]}: {title}\n{fields[1]}: {question}\n{fields[2]}: {answer} \n{fields[3]}: [\INST]' 
+    
     if model=="text-davinci-003" or model=="text-davinci-002":
-        text = openai.Completion.create(
+        print(general_prompt)
+        general_text = openai.Completion.create(
             model=model,
-            prompt=guided_prompt,
-            # max_tokens=len(enc.encode(q2)),
+            prompt=general_prompt,
+            max_tokens=max_tokens,
             temperature=0
         )
-        return text['choices'][0]['text']
+        print(general_text['choices'][0]['text'])
+        print("=========================================================")
+        print(guided_prompt)
+        guided_text = openai.Completion.create(
+            model=model,
+            prompt=guided_prompt,
+            max_tokens=max_tokens,
+            temperature=0
+        )
+        print(guided_text['choices'][0]['text'])
+
+        return general_text['choices'][0]['text'], guided_text['choices'][0]['text']
 
     else:
-        response = openai.ChatCompletion.create(
+        print(general_prompt)
+        general_response = openai.ChatCompletion.create(
+              model=model,
+              messages=[
+                  {"role": "system", "content": "You are a chatbot"},
+                  {"role": "user", "content": general_prompt},
+              ],
+              max_tokens=max_tokens,
+              temperature=0 # to ensure reproducibility
+          )
+
+        general_result = ''
+        for choice in general_response.choices:
+            general_result += choice.message.content
+        print(general_result)
+        print("=========================================================")
+        print(guided_prompt)
+        guided_response = openai.ChatCompletion.create(
               model=model,
               messages=[
                   {"role": "system", "content": "You are a chatbot"},
                   {"role": "user", "content": guided_prompt},
               ],
-            #   max_tokens=len(enc.encode(q2)),
+              max_tokens=max_tokens,
               temperature=0 # to ensure reproducibility
           )
 
-        result = ''
-        for choice in response.choices:
-            result += choice.message.content
-        return result
+        guided_result = ''
+        for choice in guided_response.choices:
+            guided_result += choice.message.content
+        print(guided_result)
+
+        return general_result, guided_result
 
 if __name__ == "__main__":
     year = sys.argv[1] ### the only param of the script is the year we want to analyse
@@ -95,25 +122,57 @@ if __name__ == "__main__":
     elif year=="2022":
         fields = ('query', 'question', 'answer', 'background')
 
-    topics = load_benchmark("2021", fields)
+    topics = load_benchmark(year, fields)
 
-    lev, bleu, r = [], [], []
+    lev_gen, bleu_gen, r_gen, lev_guid, bleu_guid, r_guid = [], [], [], [], [], []
+    count_leven, count_bleu, count_rouge = 0, 0, 0
     for topic in topics:
-        compl = complete("2021", topic[0], topic[1], topic[2], fields)    
-        print(compl)
-        print("Real value:```", topic[3],"```")
+        real_narrative = topic[3] 
+
+        if "llama" in model:
+            tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-13b-chat-hf")
+            tokens = len(tokenizer.encode(real_narrative))
+        else:
+            enc = tiktoken.encoding_for_model(model)
+            tokens = len(enc.encode(real_narrative))
+
+        general, guided = complete(year, topic[0], topic[1], topic[2], fields, tokens) 
+        print(f"Real value:\"{real_narrative}\"")
         #### AQUI DUPLICAR LA SIMILARIDAD
-        leven,bleurt,rou = sim(compl,topic[3])
-        print("Levenshtein:",leven, "BLEURT:",bleurt, "ROUGE:", rou)
+        leven_gen,bleurt_gen,rou_gen = sim(general,topic[3])
+        print("GENERAL--- Levenshtein:",leven_gen, "BLEURT:",bleurt_gen, "ROUGE:", rou_gen)
+        leven_guid,bleurt_guid,rou_guid = sim(guided,topic[3])
+        print("GUIDED---- Levenshtein:",leven_guid, "BLEURT:",bleurt_guid, "ROUGE:", rou_guid)
         print()
-        lev.append(leven)
-        bleu.append(bleurt)
-        r.append(rou)
-        ###### AQUI TESTS STATS
+
+        if leven_guid > leven_gen:
+            count_leven+=1
+
+        if bleurt_guid > bleurt_gen:
+            count_bleu+=1
+
+        if rou_guid > rou_gen:
+            count_rouge+=1
+
+        lev_gen.append(leven_gen)
+        bleu_gen.append(bleurt_gen)
+        r_gen.append(rou_gen)
+        lev_guid.append(leven_guid)
+        bleu_guid.append(bleurt_guid)
+        r_guid.append(rou_guid)
+        print()
+        print()
+
+    ###### AQUI TESTS STATS!!!! 
+    print("ALL GENERAL--- Levenshtein:",np.mean(lev_gen), "BLEURT:",np.mean(bleu_gen), "ROUGE:", np.mean(r_gen))
+    print("ALL GUIDED--- Levenshtein:",np.mean(lev_guid), "BLEURT:",np.mean(bleu_guid), "ROUGE:", np.mean(r_guid))
+    print(f"{count_leven}, {count_bleu}, {count_rouge}")
     
-    print("ALL Levenshtein:",np.mean(leven), "BLEURT:",np.mean(bleu), "ROUGE:", np.mean(r))
-    # if rat>threshold:
-    #     matches+=1
-    
-    # print(matches)
-    # print(matches/len(topics))
+    if np.mean(lev_guid) > np.mean(lev_gen):
+        print("Wilcoxon Levensthein", wilcoxon(lev_guid, lev_gen, alternative='greater'))
+
+    if np.mean(bleu_guid) > np.mean(bleu_gen):
+        print("Wilcoxon BLEURT", wilcoxon(bleu_guid, bleu_gen, alternative='greater'))
+
+    if np.mean(r_guid) > np.mean(r_gen):
+        print("Wilcoxon ROUGE", wilcoxon(r_guid, r_gen, alternative='greater'))
